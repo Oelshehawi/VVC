d604b76e26c78d73e07e8b2b636ebb31877629c9
oelsheha <oelsheha@sfu.ca>
2023-08-13 21:13:33 -0700

    Added beginning of Service seciton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,45 +2,84 @@
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import NextImage from 'next/image';
 import styles from './page.module.css';
+import { FaAngleRight } from 'react-icons/fa';
 
 export default function Home() {
   return (
     <>
-      <header className={styles.headerContainer}>
-        <Container fluid>
-          <Row className="align-items-center">
-            <Col
-              md={6}
-              className={`rounded-end text-center mt-4 mt-md-0 ${styles.headerContent}`}
+      <Container>
+        <Row className="align-items-center">
+          <Col
+            md={6}
+            className={`rounded-end mt-4 mt-md-0 ${styles.headerContent}`}
+          >
+            <h1 className={`${styles.gradientText}`}>
+              Clean. Safe. Certified.
+            </h1>
+            <p>
+              Ensure your kitchen is safe, compliant, and running at its best
+              with our top-quality cleaning and certification solutions.
+            </p>
+            <Button
+              className={`rounded-5 text-center ${styles.primaryButton}`}
+              variant="primary"
+              size="lg"
             >
-              <h1 className={`${styles.gradientText}`}>
-                Clean. Safe. Certified.
-              </h1>
+              Get a Free Quote
+            </Button>
+          </Col>
+          <Col md={6} className="mt-4">
+            <NextImage
+              alt="Clean Kitchen"
+              src="/commercialKitchen.jpg"
+              layout="responsive"
+              width="300"
+              height="200"
+              className="rounded"
+            />
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid className={styles.featureSection}>
+        <Container className={`my-5 ${styles.featureSection}`}>
+          <Row className="aling-items-center my-4 py-4">
+            <Col md={6} className="d-flex align-items-center">
+              <h1> Ensuring Clean and Safe Commercial Kitchens</h1>
+            </Col>
+            <Col md={6}>
               <p>
-                Ensure your kitchen is safe, compliant, and running at its best
-                with our top-quality cleaning and certification solutions.
+                We understand the importance of kitchen exhaust cleaning and
+                certificaiton for commercial kitchens. Our certified technicians
+                are trained to thoroughly clean and inspect kitchen exhaust
+                systems, ensuring that they are free from grease buildup and
+                potential fire hazards.
               </p>
               <Button
-                className={`rounded-5 text-center ${styles.primaryButton}`}
-                variant="primary"
-                size="lg"
+                variant="outline-primary"
+                className={`${styles.secondaryButton}`}
               >
-                Get a Free Quote
+                Learn More
+              </Button>
+              <Button
+                variant="outline-light"
+                className={`m-2 ${styles.ternaryButton}`}
+              >
+                Contact Us <FaAngleRight></FaAngleRight>
               </Button>
             </Col>
-            <Col className="p-4">
-              <NextImage
-                alt="Clean Kitchen"
-                src="/commercialKitchen.jpg"
-                layout="responsive"
-                width="700"
-                height="500"
-                className="rounded"
-              />
-            </Col>
           </Row>
+          <Col>
+            <NextImage
+              alt="Clean Kitchen"
+              src="/commercialKitchen.jpg"
+              layout="responsive"
+              width="600"
+              height="400"
+              className="rounded"
+            />
+          </Col>
         </Container>
-      </header>
+      </Container>
     </>
   );
 }
